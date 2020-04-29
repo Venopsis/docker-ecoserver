1410f13e51f7ea1bf3c82739ccc54eee88c37480
Venopsis <949573+Venopsis@users.noreply.github.com>
2020-04-30 00:01:35 +0200

    Edited entrypoint to add Init option (do not start the ecoserver for initial configuration)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ "$#" -eq "0" ]
+if [ "$#" -eq "Init" ]
+    then
+        # Does nothing to let user manage config file before first run.
+elif [ "$#" -eq 0 ]
     then
         # Start in Tmux session for late management
         tmux new-session -d -s "ecoserver" mono EcoServer.exe -nogui
-    else
-        echo "This container does not accept arguments"
-        exit 1
+else
+    echo "This container does not accept arguments"
+    exit 1
 
 exec "$@"
