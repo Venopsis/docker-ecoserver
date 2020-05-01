feb7e4b9599ecef28155cafe196a4c8464c9b4d6
Venopsis <949573+Venopsis@users.noreply.github.com>
2020-05-01 16:31:21 +0200

    test run only bash to access container content and ensure entrypoint is working

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-if [ "$#" -eq "Init" ]
-    then
-        # Does nothing to let user manage config file before first run.
-fi
-
-if [ "$#" -eq "0" ]
-    then
-        # Start in Tmux session for late management
-        tmux new-session -d -s "ecoserver" mono EcoServer.exe -nogui
-else
-    echo "Only Init for first run or nothing is accepted as arguments."
-    exit 1
-fi
-
-exec "$@"
+bash
