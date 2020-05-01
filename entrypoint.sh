e58f750e8567e0e1dbb720de77c9f411987addba
Venopsis <949573+Venopsis@users.noreply.github.com>
2020-05-01 16:09:13 +0200

    Edited if condition in entrypoint to fix error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-echo world
+
+if [ "$#" -eq "Init" ]
+    then
+        # Does nothing to let user manage config file before first run.
+fi
+
+if [ "$#" -eq "0" ]
+    then
+        # Start in Tmux session for late management
+        tmux new-session -d -s "ecoserver" mono EcoServer.exe -nogui
+else
+    echo "Only Init for first run or nothing is accepted as arguments."
+    exit 1
+fi
+
+exec "$@"
