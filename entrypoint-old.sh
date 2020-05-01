4b38e56bae83e6885a8fb2cd1ef0d42ab9843411
Venopsis <949573+Venopsis@users.noreply.github.com>
2020-05-01 15:28:26 +0200

    testing entrypoint

---
+++ entrypoint-old.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+if [ "$#" -eq "Init" ]
+    then
+        # Does nothing to let user manage config file before first run.
+elif [ "$#" -eq "0" ]
+    then
+        # Start in Tmux session for late management
+        tmux new-session -d -s "ecoserver" mono EcoServer.exe -nogui
+else
+    echo "This container does not accept arguments"
+    exit 1
+fi
+
+exec "$@"
