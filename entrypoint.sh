551f9dacad450a5c5604fec2f6901e6b13d1e066
Venopsis <949573+Venopsis@users.noreply.github.com>
2020-05-01 20:14:09 +0200

    Verbosing Start option in entrypoint to check what's happening after tmux command runned as the container stop.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,16 @@ if [ "$1" = "Init" ]
         bash
 elif [ "$1" = "Start" ]
     then
+        echo "Go into option Start"
         # Start in Tmux session for late management
+        echo "Run tmux command"
         tmux new-session -d -s "ecoserver" "mono EcoServer.exe -nogui"
+        echo "Leaving tmux"
+        echo "execute tail command"
+        tail -f /dev/null
+        echo "leaving tail command"
 else
     echo "This container does not accept arguments"
     exit 1
 fi
+echo "Out of else if loop"
